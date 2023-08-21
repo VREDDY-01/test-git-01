64fdbd22d79f8e751dfd7338a7ad3da6573ea819
K Vishnu Teja <vreddy2300@gmail.com>
2023-08-22 02:48:34 +0530

    exception handled

--- Exception-Handling/Practice/first_intellij/src/Main.java
+++ Exception-Handling/Practice/first_intellij/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(1/0);
+        try{
+            System.out.println(1/0);
+        }catch (Exception e){
+            System.out.println(e + " is handled");
+        }
     }
 }
